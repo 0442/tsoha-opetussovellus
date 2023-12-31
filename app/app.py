52922a12888618adf5efeeecb6029ec96e6a998c
82746 <82746@proton.me>
2024-01-01 01:20:25 +0200

    Add x_prefix=1

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 app.config['APPLICATION_ROOT'] = '/'
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 from .routes.routes import *
