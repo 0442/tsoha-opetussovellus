917fb4bda086c8d594c4e725334ffba0269d14b2
82746 <82746@proton.me>
2023-12-31 23:28:15 +0200

    Add proxy fix

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
-from os import getenv
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
+app.config['APPLICATION_ROOT'] = '/'
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 
 from .routes.routes import *
